6c40499b4a1e1740561974a1e58742be5d2ba178
Michael Bestas <mikeioannina@gmail.com>
2015-01-28 02:04:48 +0200

    Remove deprecated VideoEditor packages
    Change-Id: I47b1d17e6b93dae6992fe169d034bfa4eb820370
    
    Conflicts:
    	config/common_full.mk

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,9 +1,6 @@
 # Inherit common OSE stuff
 $(call inherit-product, vendor/ose/config/common.mk)
 
-# Bring in all video files
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
-
 # Include OSE LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/ose/overlay/dictionaries
 
@@ -13,14 +10,6 @@ PRODUCT_PACKAGES += \
     PhaseBeam \
     PhotoTable \
     PhotoPhase
-
-PRODUCT_PACKAGES += \
-    VideoEditor \
-    libvideoeditor_jni \
-    libvideoeditor_core \
-    libvideoeditor_osal \
-    libvideoeditor_videofilters \
-    libvideoeditorplayer
 
 # Extra tools in OSE
 PRODUCT_PACKAGES += \
